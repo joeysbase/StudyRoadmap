3e49709008fcfec4b789160084e5e03485671782
石钟懿 <shizhongyi@ishis-MacBook-Air.local>
2026-01-11 17:41:32 -0800

    update readme

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,14 +2,14 @@ import { useState } from "react"
 
 export default function NavBar() {
     return (
-        <nav className="flex h-12 px-10 py-2 bg-blue-500">
+        <nav className="flex h-12 px-20 py-2 bg-blue-500">
             <div className="flex grow items-center text-white">
-                <div className=" p-1 mx-1 font-[cursive] text-2xl">StudyGo</div>
+                <div className=" p-1 mx-1 font-[cursive] text-2xl">StudyRoadmap</div>
                 <div className=" p-1 mx-1 rounded-xl hover:bg-white hover:text-black hover:cursor-default">Explore</div>
                 <div className=" p-1 mx-1 hover:bg-white hover:text-black rounded-xl hover:cursor-pointer">My Learning</div>
             </div>
             <div className="flex grow items-center justify-center text-white">
-                <input className='p-1 mx-1 rounded-xl w-70 bg-white text-black' type="text" name="" id="" placeholder="Search for course"/>
+                <input className='p-1 mx-1 rounded-xl w-6/10 bg-white text-black' type="text" name="" id="" placeholder="Search for course"/>
                 <button className="border p-1 mx-1 rounded-xl hover:bg-white hover:text-black hover:cursor-pointer" type="button">Search</button>
             </div>
             <div className="flex grow items-center justify-end text-white">
